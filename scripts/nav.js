ecb18dd0f57446d456641a8bb60dbf209893eb7d
Michael Obafemi <mihaelmais@gmail.com>
2018-10-31 22:03:00 -0400

    first commit

---
+++ scripts/nav.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#open").on("click", function () {
+    $("#overlay").removeClass("close");
+  });
+  $("#close").on("click", function () {
+    $("#overlay").addClass("close");
+  });
+});
